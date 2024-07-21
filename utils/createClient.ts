1d4f349c62977dd16a56498cb8f2ecddd0a39ac2
Guilherme Nascimento Guimaraes <134396918+gguimaraesn@users.noreply.github.com>
2024-07-21 19:17:33 -0300

    fix: I hid the Supabase details

--- utils/createClient.ts
+++ utils/createClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-    "https://hpmvqprmsyhetkjqhylh.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwbXZxcHJtc3loZXRranFoeWxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE0MTc3MzMsImV4cCI6MjAzNjk5MzczM30.E1x5wuAJTf3uJtquTWUEWYlHqXFq5ajpEVJAqSnhAkQ"
+    "*****",
+    "*****"
      ) 
